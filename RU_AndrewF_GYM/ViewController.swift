6b79fa88c93b5e71a19ad701327224f69ae83594
Andrew Frolov <adm.kolchak.221001@gmail.com>
2020-07-22 22:07:37 +0700

    start

---
+++ RU_AndrewF_GYM/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  RU_AndrewF_GYM
+//
+//  Created by Sergio Ramos on 22/07/2020.
+//  Copyright © 2020 Sergio Ramos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
---
+++ RU_AndrewF_GYMTests/RU_AndrewF_GYMTests.swift
@@ -0,0 +1,34 @@
+//
+//  RU_AndrewF_GYMTests.swift
+//  RU_AndrewF_GYMTests
+//
+//  Created by Sergio Ramos on 22/07/2020.
+//  Copyright © 2020 Sergio Ramos. All rights reserved.
+//
+
+import XCTest
+@testable import RU_AndrewF_GYM
+
+class RU_AndrewF_GYMTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
